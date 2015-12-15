94951f2ad725c3cbdbfccb5ffc2d30d0017e9d2d
tom2tom <tpgww@onepost.net>
2015-12-15 13:16:17 +1100

    change getKeywords API also prefer cached keywords

--- lib/class.SEO_keyword.php
+++ lib/class.SEO_keyword.php
@@ -7,6 +7,9 @@
 class SEO_keyword
 {
 	private function _get_keywords($source, $sep = ' ', $minlength = 6) {
+		if (!$source) {
+			return array();
+		}
 		//TODO not utf8_decode() >> iconv()?
 		$source = preg_replace('/\{[^\}]+\}/isU', '', utf8_decode($source));
 		$source = str_replace("\n",' ',strip_tags($source));
@@ -29,121 +32,119 @@ class SEO_keyword
 		return array_filter($keywords,'strlen');
 	}
 
-	private function _get_headlines($file) {
-		$h1tags = preg_match_all("/(<h1.*>)(\w.*)(<\/h1>)/isxmU",$file,$patterns);
-		$content = "";
-		foreach($patterns[2] as $tag) {
-			$content .= " ".$tag;
-		}
-		$h2tags = preg_match_all("/(<h2.*>)(\w.*)(<\/h2>)/isxmU",$file,$patterns);
-		foreach($patterns[2] as $tag) {
-			$content .= " ".$tag;
-		}
-		$h3tags = preg_match_all("/(<h3.*>)(\w.*)(<\/h3>)/isxmU",$file,$patterns);
-		foreach($patterns[2] as $tag) {
-			$content .= " ".$tag;
-		}
-		$h4tags = preg_match_all("/(<h4.*>)(\w.*)(<\/h4>)/isxmU",$file,$patterns);
-		foreach($patterns[2] as $tag) {
-			$content .= " ".$tag;
-		}
-		$h5tags = preg_match_all("/(<h5.*>)(\w.*)(<\/h5>)/isxmU",$file,$patterns);
-		foreach($patterns[2] as $tag) {
-			$content .= " ".$tag;
-		}
-		$h6tags = preg_match_all("/(<h6.*>)(\w.*)(<\/h6>)/isxmU",$file,$patterns);
-		foreach($patterns[2] as $tag) {
-			$content .= " ".$tag;
-		}
-		return $content;
-	}
-
-	public function getKeywordSuggestions($mod,$content_id = FALSE,$content = NULL) {
-		$gCms = cmsms();
+	public function getKeywords($mod, $content_id = FALSE, $content = NULL, $savedwords = FALSE) {
+		$gCms = cmsms(); //CMSMS 1.8+
 		if ($content == NULL) {
-			$contentops = $gCms->GetContentOperations();
-			if (!$content = $contentops->LoadContentFromId ($content_id)) {
-				return '';
+			$content = $gCms->GetContentOperations()->LoadContentFromId ($content_id);
+			if (!$content) {
+				return array();
 			}
 		}
-		elseif ($content_id == FALSE)
+		elseif ($content_id == FALSE) {
 			$content_id = (int)$content->Id();
+		}
 
-		$word_len = $mod->GetPreference('keyword_minlength',6);
-		$word_minwt = $mod->GetPreference('keyword_minimum_weight',7);
-		$propname = str_replace(' ','_',$mod->GetPreference('keyword_block',''));
 		$db = $gCms->GetDb();
-		$query = 'SELECT content FROM '.cms_db_prefix().'content_props WHERE content_id=? AND prop_name=?';
+		$pre = cms_db_prefix();
+		if ($savedwords === FALSE) {
+			$savedwords = $db->GetOne(
+			 'SELECT keywords FROM '.$pre.'module_seotools WHERE content_id=?',
+			 array($content_id));
+		}
+		
+		$wlen = $mod->GetPreference('keyword_minlength',6);
+		$minwt = $mod->GetPreference('keyword_minimum_weight',7);
+		$sep = $mod->GetPreference('keyword_separator',' ');
+		$propname = str_replace(' ','_',$mod->GetPreference('keyword_block',''));
+		$query = 'SELECT content FROM '.$pre.'content_props WHERE content_id=? AND prop_name=?';
 
-		$other_keywords = array();
+		$got_keywords = array();
 		/* Try for explicit stored keywords */
-		$stored_keywords = $db->GetOne($query,array($content_id,$propname));
+		if ($savedwords) {
+			$stored_keywords = $savedwords.$sep;
+		}
+		else {
+			$stored_keywords = '';
+		}
+		$stored_keywords .= $db->GetOne($query,array($content_id,$propname));
 		if ($stored_keywords) {
-			$sep = $mod->GetPreference('keyword_separator',' ');
-			$stored_keywords = self::_get_keywords($stored_keywords,$sep,$word_len);
+			$stored_keywords = self::_get_keywords($stored_keywords,$sep,$wlen);
 			if ($stored_keywords) {
 				foreach($stored_keywords as $keyword) {
-					$other_keywords[$keyword] = $word_minwt;
+					$got_keywords[$keyword] = $minwt; //allow all except dup's
 				}
 			}
 		}
-		if (!$other_keywords) {
-			/* Generate keywords from page title, description and content */
+		if (!$got_keywords) {
+			/* Revert to keywords derived from page title, description and content */
 			$page_name = $content->Name();
 			if ($page_name) {
 				$wt = $mod->GetPreference('keyword_title_weight',6);
-				$title_keywords = self::_get_keywords($page_name,' ',$word_len);
+				$title_keywords = self::_get_keywords($page_name,' ',$wlen);
 				foreach($title_keywords as $keyword) {
-					if (!isset($other_keywords[$keyword])) {
-						$other_keywords[$keyword] = 0;
+					if (!isset($got_keywords[$keyword])) {
+						$got_keywords[$keyword] = 0;
 					}
-					$other_keywords[$keyword] += $wt;
+					$got_keywords[$keyword] += $wt;
 				}
 			}
 			$propname = str_replace(' ','_',$mod->GetPreference('description_block',''));
 			$description = $db->GetOne($query,array($content_id,$propname));
 			if ($description) {
 				$wt = $mod->GetPreference('keyword_description_weight',4);
-				$description_keywords = self::_get_keywords($description,' ',$word_len);
+				$description_keywords = self::_get_keywords($description,' ',$wlen);
 				foreach($description_keywords as $keyword) {
-					if (!isset($other_keywords[$keyword])) {
-						$other_keywords[$keyword] = 0;
+					if (!isset($got_keywords[$keyword])) {
+						$got_keywords[$keyword] = 0;
 					}
-					$other_keywords[$keyword] += $wt;
+					$got_keywords[$keyword] += $wt;
 				}
 			}
 			$content = $db->GetOne($query,array($content_id,'content_en'));
 			if ($content) {
-				$wt = $mod->GetPreference('keyword_headline_weight',2);
-				$headline_keywords = self::_get_keywords(self::_get_headlines($content),' ',$word_len);
-				foreach($headline_keywords as $keyword) {
-					if (!isset($other_keywords[$keyword])) {
-						$other_keywords[$keyword] = 0;
+				$heads = '';
+				for ($i = 1; $i < 7; $i++) {
+					if (preg_match_all("/(<h{$i}.*>)(\w.*)(<\/h{$i}>)/isxmU",$content,$patterns)) {
+						if ($heads) $heads .= ' ';
+						$heads .= implode(' ',$patterns[2]);
 					}
-					$other_keywords[$keyword] += $wt;
+				}
+				if ($heads) {
+					$wt = $mod->GetPreference('keyword_headline_weight',2);
+					$headline_keywords = self::_get_keywords($heads,' ',$wlen);
+					foreach($headline_keywords as $keyword) {
+						if (!isset($got_keywords[$keyword])) {
+							$got_keywords[$keyword] = 0;
+						}
+						$got_keywords[$keyword] += $wt;
+					}
 				}
 				$wt = $mod->GetPreference('keyword_content_weight',1);
-				$content_keywords = self::_get_keywords($content,' ',$word_len);
+				$content_keywords = self::_get_keywords($content,' ',$wlen);
 				foreach($content_keywords as $keyword) {
-					if (!isset($other_keywords[$keyword])) {
-						$other_keywords[$keyword] = 0;
+					if (!isset($got_keywords[$keyword])) {
+						$got_keywords[$keyword] = 0;
 					}
-					$other_keywords[$keyword] += $wt;
+					$got_keywords[$keyword] += $wt;
 				}
 			}
-			arsort($other_keywords);
+			arsort($got_keywords);
+			//cache derived keywords (until next page-content-change)
+			$query = 'UPDATE '.$pre.'module_seotools SET keywords=? WHERE content_id=?';
+			$merged = implode($sep,array_keys($got_keywords));
+			$db->Execute($query,array($merged,$content_id));
 		}
 
 		//TODO not utf8_decode() >> iconv()?
 		$exclude_list = utf8_decode($mod->GetPreference('keyword_exclude',''));
-		foreach ($other_keywords as $key=>&$value) {
-			if ($value < $word_minwt || ($exclude_list && strpos($key,$exclude_list) !== FALSE)) {
+		foreach ($got_keywords as $key=>&$value) {
+			if ($value < $minwt || ($exclude_list && strpos($key,$exclude_list) !== FALSE)) {
 				$value = '';
 			}
 		}
 		unset($value);
 
-		return array_keys(array_filter($other_keywords,'is_numeric'));
+		return array_keys(array_filter($got_keywords,'is_numeric'));
 	}
 }
 
