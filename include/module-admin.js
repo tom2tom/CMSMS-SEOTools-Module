17cdbd17bc3851509ff64a8cbe625460e5432f3a
tom2tom <tpgww@onepost.net>
2015-12-19 15:48:48 +1100

    display help via show/hide div, not custom tooltip

--- include/module-admin.js
+++ include/module-admin.js
@@ -1,54 +1,32 @@
 function select_all(name) {
  var st = $('#all'+name).attr('checked');
- if(!st) st = false;
+ if(!st) { st = false; }
  $('input[name="'+name+'sel[]"][type="checkbox"]').attr('checked', st);
 }
 function confirm_click(name) {
  var cb = $('input[name='+name+'sel[]]:checked');
  return (cb.length > 0);
 }
-function tipup() {
- var $t = $(this),
-  h = $t.height() + 5,
-  p = $t.offset(),
-  title = $t.attr('title'),
-  $n = $('<p class="tooltip"></p>');
- $t.removeAttr('title').data('tipText',title);
- $n.text(title)
-   .appendTo('body')
-   .css({ top: p.top + h, left: p.left + 5})
-   .fadeIn(400);
- $t.data('tipNew',$n);
+function tipkeep() {
+ $(this).css('display','block'); //TODO timer
 }
-
-function tipup2() {
- try {
-  if (this == document.activeElement) {
- var dbg = 1;
- //TODO manipulate tip
-    return;
-  }
- } catch(err) {}
+function tipsee() {
+ $(this).parent().next('.slidediv').slideDown(300);
 }
-
-function tipdown() {
- var $t = $(this);
- $t.attr('title',$t.data('tipText')).data('tipNew').fadeOut(200,function(){
-  $t.data('tipNew').remove();
- });
+function tiphide() {
+ $(this).parent().next('.slidediv').slideUp({ duration : 200, queue : false });
 }
-
-function tipdown2() {
- try {
-  if (this == document.activeElement) {
- var dbg = 1;
- //TODO manipulate tip
-    return;
-  }
- } catch(err) {}
+function tiphide2() {
+ var a = document.activeElement,
+   n = a.tagName.toUpperCase();
+ if (n == 'BODY' || a != this) {
+  $(this).parent().next('.slidediv').slideUp({ duration : 200, queue : false });
+ }
 }
 
 $(document).ready(function(){
+ $('.slidediv').css('display','none').children().click(tipkeep);
+ $('.slidetip').children().filter(':input').hover(tipsee,tiphide2).focus(tipsee).blur(tiphide);
  $('#alerts_c a').click(function(){
   var indx = $(this).attr('class').substring(1);
   //this relies on undocumented tabs-structure for admin pages
@@ -60,5 +38,4 @@ $(document).ready(function(){
   $('#'+to+'_c').css('display','block');
   return false;
  });
- $('[title]').hover(tipup,tipdown).children().filter(':input').focus(tipup2).blur(tipdown2);
 });
