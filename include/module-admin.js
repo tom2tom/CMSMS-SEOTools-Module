c90bcf4f063227fc2bda9fe5f1fa170b2861260a
tom2tom <tpgww@onepost.net>
2015-12-19 11:32:29 +1100

    revise tip management

--- include/module-admin.js
+++ include/module-admin.js
@@ -3,10 +3,49 @@ function select_all(name) {
  if(!st) st = false;
  $('input[name="'+name+'sel[]"][type="checkbox"]').attr('checked', st);
 }
-
 function confirm_click(name) {
  var cb = $('input[name='+name+'sel[]]:checked');
  return (cb.length > 0);
+}
+function tipup() {
+ var $t = $(this),
+  h = $t.height() + 5,
+  p = $t.offset(),
+  title = $t.attr('title'),
+  $n = $('<p class="tooltip"></p>');
+ $t.removeAttr('title').data('tipText',title);
+ $n.text(title)
+   .appendTo('body')
+   .css({ top: p.top + h, left: p.left + 5})
+   .fadeIn(400);
+ $t.data('tipNew',$n);
+}
+
+function tipup2() {
+ try {
+  if (this == document.activeElement) {
+ var dbg = 1;
+ //TODO manipulate tip
+    return;
+  }
+ } catch(err) {}
+}
+
+function tipdown() {
+ var $t = $(this);
+ $t.attr('title',$t.data('tipText')).data('tipNew').fadeOut(200,function(){
+  $t.data('tipNew').remove();
+ });
+}
+
+function tipdown2() {
+ try {
+  if (this == document.activeElement) {
+ var dbg = 1;
+ //TODO manipulate tip
+    return;
+  }
+ } catch(err) {}
 }
 
 $(document).ready(function(){
@@ -21,23 +60,5 @@ $(document).ready(function(){
   $('#'+to+'_c').css('display','block');
   return false;
  });
- $('.masterTooltip').hover(function(){
-  var $t = $(this),
-   h = $t.height() + 5,
-   p = $t.offset(),
-   title = $t.attr('title'),
-   $n = $('<p class="tooltip"></p>');
-  $t.removeAttr('title').data('tipText',title);
-  $n.text(title)
-   .appendTo('body')
-   .css({ top: p.top + h, left: p.left + 5})
-   .fadeIn(500);
-  $t.data('tipNew',$n);
- }, function() {
-   $t = $(this);
-   $t.attr('title',$t.data('tipText'))
-     .data('tipNew').fadeOut(200,function(){
-       $t.data('tipNew').remove();
-   });
- });
+ $('[title]').hover(tipup,tipdown).children().filter(':input').focus(tipup2).blur(tipdown2);
 });
